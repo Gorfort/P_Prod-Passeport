a6b11a638195eafe7aaa4b5e999d5ca5365c33f7
yohcard <cardisyohan@gmail.com>
2024-10-31 09:41:05 +0100

    mise a jour d'app

--- nodejswebshop/app/routes/User.mjs
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { get, authenticate } from "../controllers/UserController.mjs";
-
-const router = express.Router();
-
-router.get("/:username", get);
-router.post("/login", authenticate);
-
-export default router;
